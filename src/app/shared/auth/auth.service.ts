8f159db6a02bdacca1a72ff6605dc4ff6c57db96
Martin Carre <carre.martin@gmail.com>
2024-09-10 21:58:12 +0200

    Created signup and signin.

--- src/app/shared/auth/auth.service.ts
+++ src/app/shared/auth/auth.service.ts
@@ -1,70 +1,23 @@
-// import { inject, Injectable, signal } from '@angular/core';
-// import { MatSnackBar } from '@angular/material/snack-bar';
-// import { AuthedUser } from './authed-user.interface';
-// import { Router } from '@angular/router';
-// import { mockAuthUsers } from '../../user/mock-users';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class AuthService {
-//   private router = inject(Router);
-//   private aUser = signal<AuthedUser | null>(null);
-//   private _snackbar = inject(MatSnackBar);
-//   authedUser = this.aUser.asReadonly();
-
-//   signup(signupData: any) {
-//     const isAuthedUser = this.authedUserHandler();
-//     if (isAuthedUser) {
-//       return;
-//     }
-//     console.log('Signup data:', signupData);
-//   }
-
-//   login(email: string, password: string) {
-//     const isAuthedUser = this.authedUserHandler();
-//     if (isAuthedUser) {
-//       console.log(this.aUser());
-//       return;
-//     }
-//     const foundUser = mockAuthUsers.find(user => user.email === email);
-//     if (!foundUser) {
-//       alert('User not found. Please sign up.');
-//       this.router.navigate(['/user/signup']);
-//       return;
-//     } else {
-//       this.aUser.set(foundUser);
-//       this._snackbar.open(`Login successful! Welcome ${this.aUser()?.name}.`, 'Close', { horizontalPosition: 'center', verticalPosition: 'top', duration: 5000 });
-//       console.log('Login:', this.aUser());
-//     }
-//   }
-
-//   logout() {
-//     this.aUser.set(null);
-//     this.router.navigate(['/']);
-//     this._snackbar.open('Loggout successful! See you later...', 'Close', { horizontalPosition: 'center', verticalPosition: 'top', duration: 5000 });
-//     console.log('Logout: ', this.aUser());
-//   }
-
-//   authedUserHandler(): boolean { 
-//     if (this.aUser()) {
-//       this._snackbar.open('User already signed in. You can\'t perform this action again... Try logging out first.', 'Close', { horizontalPosition: 'center', verticalPosition: 'top', duration: 5000 });
-//       this.router.navigate(['/']);
-//       return true;
-//     } else {
-//       return false;
-//     }
-//   }
-// }
-
 import { isPlatformBrowser } from '@angular/common';
 import { initializeApp, FirebaseApp } from 'firebase/app';
 import { inject, Injectable, PLATFORM_ID, signal } from '@angular/core';
-import { getAuth, signInWithEmailAndPassword, User, UserCredential, signOut, Auth, connectAuthEmulator } from 'firebase/auth';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, User, UserCredential, signOut, Auth, connectAuthEmulator } from 'firebase/auth';
 
 import { environment } from '../../../environments/environment';
 import { AuthedUser } from './authed-user.interface';
 import { UserRole } from './user-role.interface';
+
+interface SignupInfo { 
+  email: string;
+  password: string;
+  confirmPassword: string;
+  name: string;
+  role: string;
+  useCase: string;
+  businessWebsite: string;
+  businessName: string;
+  businessType: string;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -74,9 +27,8 @@ export class AuthService {
   private auth: Auth | null = null;
   private platformId = inject(PLATFORM_ID);
 
-  private aUser = signal<AuthedUser | null>(null); //TODO: Replace any with a proper type
+  private aUser = signal<AuthedUser | null>(null);
   authedUser = this.aUser.asReadonly();
-
 
   constructor() {
     if (isPlatformBrowser(this.platformId)) {
@@ -90,7 +42,7 @@ export class AuthService {
       console.log('Using production Firebase Auth');
     }
     if (!this.app) {
-      throw new Error('Firebase app not initialized');
+      console.error('Firebase app not initialized');
     }
   }
 
@@ -98,7 +50,7 @@ export class AuthService {
     signInWithEmailAndPassword(this.auth!, email, password)
       .then((userCredential: UserCredential) => {
         if (!userCredential.user) {
-          throw new Error('User not found');
+          console.error('No user found');
         } else if (userCredential.user) {
           const resUser: User = userCredential.user;
           const resAuthedUser: AuthedUser = { 
@@ -107,7 +59,6 @@ export class AuthService {
             isVerified: resUser.emailVerified,
             roles: [UserRole.Admin]
           }
-          // TODO: transform userCredential.user to AuthedUser
           this.aUser.set(resAuthedUser);
           console.log('User:', this.aUser());
         }
@@ -122,8 +73,26 @@ export class AuthService {
     this.aUser.set(null);
   }
 
-  signup(signupData: any) {
-    console.log('Signup data:', signupData);
+  signup(signupData: SignupInfo) {
+    if (this.auth) {
+      createUserWithEmailAndPassword(this.auth, signupData.email, signupData.password)
+        .then((userCredential: UserCredential) => {
+          const resUser: User = userCredential.user;
+          const resAuthedUser: AuthedUser = { 
+            email: resUser.email ? resUser.email : 'Anonymous',
+            name: resUser.displayName ? resUser.displayName : 'Anonymous',
+            isVerified: resUser.emailVerified,
+            roles: [UserRole.Admin]
+          }
+          this.aUser.set(resAuthedUser);
+          console.log('User:', this.aUser());
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+    } else {
+      this.firebaseInitError();
+    }
   }
 
   firebaseInitError() {
