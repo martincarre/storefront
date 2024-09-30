ca65b76ef50c19a9bde54f1451b5f24f4554c4cf
Martin Carre <carre.martin@gmail.com>
2024-09-30 17:50:38 +0200

    made the pertinent imports for ngx-quill

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -13,6 +13,8 @@ import { getStorage, provideStorage } from '@angular/fire/storage';
 import { environment } from '../environments/environment';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 
+import { provideQuillConfig } from 'ngx-quill/config';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()), 
@@ -29,5 +31,26 @@ export const appConfig: ApplicationConfig = {
     provideFirestore(() => getFirestore()), 
     provideFunctions(() => getFunctions()),
     provideStorage(() => getStorage()),
+
+    provideQuillConfig({ 
+      modules: {
+        syntax: true,
+        toolbar: [
+          ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+          ['blockquote', 'code-block'],
+          [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+          [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+          [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+          [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+          [{ 'direction': 'rtl' }],                         // text direction
+          [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+          [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+          [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+          [{ 'font': [] }],
+          [{ 'align': [] }],
+          ['clean'],                                         // remove formatting button
+        ]
+      }
+    })
   ]
 };
